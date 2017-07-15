34bf639fa051268224fca5475c78ceb61a198bab
gitJucy <johnrlucy84@gmail.com>
2017-07-15 16:15:38 -0400

    style
    finished react portion of app with css styling

--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDa7OXuNaS41ZvHeQ_0zGkB5H0fO0dIcGs';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state= { videos:  [ ] };
-        YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+        this.state= {
+            videos:  [ ] ,
+            selectedVideo: null
+        };
+        this.videoSearch('Surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key:API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos:videos,
+                selectedVideo: videos[0]
+             });
         });
+
     }
     render(){
+        const videoSearch= _.debounce((term) => {this.videoSearch(term) }, 300);
         return (
             <div>
-                <SearchBar />
-                <VideoList  videos={this.state.videos} />
+                <SearchBar  onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                onVideoSelect={selectedVideo =>this.setState({selectedVideo}) }
+                 videos={this.state.videos} />
             </div>
         );
     }
